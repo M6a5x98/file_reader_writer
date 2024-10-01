511d7c84b6b1841d907664c2eed3b029b94e21c1
M6a5x98 <m6a5x98@outlook.com>
2024-10-01 18:48:48 +0200

    Update filereader.js

--- filereader.js
+++ filereader.js
@@ -1,42 +1,54 @@
 class TextFileReader {
-	constructor() {
-
-	}
-	readFile(file_path) {
-		if (typeof process !== "object") throw new Error('Cannot read file in browser');
-		else {
-			const fs = require('fs');
-			return fs.readFileSync(file_path, 'utf-8');
-		}
-	}
-	async readTextFromInput(input) {
-		const file=input.files[0];
-		const txt = await file.text();
-		var texte = await txt;
-		return texte;
-	}
+  constructor() {}
+  /**
+   *
+   * @param {string} file_path The path of the file to read (Node.js only)
+   * @returns The content of the file
+   */
+  readFile(file_path) {
+    if (typeof process !== "object")
+      throw new Error("Cannot read file in browser");
+    else {
+      const fs = require("fs");
+      return fs.readFileSync(file_path, "utf-8");
+    }
+  }
+  /**
+   *
+   * @param {HTMLInputElement} input The file input
+   * @returns The readed text
+   */
+  async readTextFromInput(input) {
+    const file = input.files[0];
+    const txt = await file.text();
+    let texte = txt;
+    return texte;
+  }
 }
 
 class TextFileWriter {
-	constructor() {
-
-	}
-	async saveAsText(text, fileName) {
-		if (window) {
-			let a = document.createElement("a");
-			let blob = new Blob([text], {
-				type: "text/plain"
-			});
-			a.href = await URL.createObjectURL(blob);
-			a.download = fileName;
-			a.click();
-		} else {
-			const fs = require('fs');
-			fs.writeFileSync(fileName,text)
-		}
-	}
+  constructor() {}
+  /**
+   *
+   * @param {string} text The text to write to the file
+   * @param {string} fileName The name of the file
+   */
+  async saveAsText(text, fileName) {
+    if (typeof window !== "undefined") {
+      let a = document.createElement("a");
+      let blob = new Blob([text], {
+        type: "text/plain",
+      });
+      a.href = URL.createObjectURL(blob);
+      a.download = fileName;
+      a.click();
+    } else {
+      const fs = require("fs");
+      fs.writeFileSync(fileName, text);
+    }
+  }
 }
 
-if (typeof window === "undefined) {
-	module.exports = {TextFileReader,TextFileWriter}
+if (typeof window === "undefined") {
+  module.exports = { TextFileReader, TextFileWriter };
 }
